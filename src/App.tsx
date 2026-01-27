e64fd833c7e11f42bee756844287edd9d32e73fc
Erdinç Tekay <erdinctekay@gmail.com>
2026-01-27 21:49:59 +0300

    fix: comment out DeprecationDrawer component from App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import { DeprecationDrawer } from "@/components/custom/v2/deprecation-drawer";
+// import { DeprecationDrawer } from "@/components/custom/v2/deprecation-drawer";
 import { OnboardingWrapper } from "@/screens/onboarding/onboarding-wrapper";
 
 function App() {
 	return (
 		<>
 			<OnboardingWrapper />
-			<DeprecationDrawer />
+			{/* <DeprecationDrawer /> */}
 		</>
 	);
 }
